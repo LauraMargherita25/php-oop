f04c543ab48752465f480827094631f2ea1f8943
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-29 15:16:49 +0200

    Add constructor

--- index.php
+++ index.php
@@ -5,19 +5,28 @@ class Movie{
     public $year;
     public $runningTime;
 
-    public function setTitle($_title) {
+    public function __construct($_title, $_year) {
         $this->title = $_title;
+        $this->year = $_year;
+    }
+
+    public function setRunningTime($_runningTime) {
+        $this->runningTime = $_runningTime;
     }
 }
 
-$backToTheFuture = new Movie();
-$backToTheFuture->setTitle('Back to the Future');
-$backToTheFuture->year = 1985;
-$backToTheFuture->runningTime = '116 minutes';
-var_dump($backToTheFuture);
+// $backToTheFuture = new Movie();
+// $backToTheFuture->title = 'Back to the Future';
+// $backToTheFuture->year = 1985;
+// $backToTheFuture->setRunningTime('116 minutes');
+// var_dump($backToTheFuture);
 
-$backToTheFuture2 = new Movie();
-$backToTheFuture2->setTitle('Back to the Future - 2');
-$backToTheFuture2->year = 1989;
-$backToTheFuture2->runningTime = '108 minutes';
-var_dump($backToTheFuture2);
+// $backToTheFuture2 = new Movie();
+// $backToTheFuture2->title = 'Back to the Future - 2';
+// $backToTheFuture2->year = 1989;
+// $backToTheFuture2->setRunningTime('108 minutes');
+// var_dump($backToTheFuture2);
+
+$backToTheFuture3 = new Movie("Back to the Future - 3", 1990);
+echo $backToTheFuture3->title;
+echo $backToTheFuture3->year;

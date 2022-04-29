ec895cc3b109787e5643227fccfbaad17071af4c
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-29 15:03:59 +0200

    Add method setTitle

--- index.php
+++ index.php
@@ -4,16 +4,20 @@ class Movie{
     public $title;
     public $year;
     public $runningTime;
+
+    public function setTitle($_title) {
+        $this->title = $_title;
+    }
 }
 
 $backToTheFuture = new Movie();
-$backToTheFuture->title = 'Back to the Future';
+$backToTheFuture->setTitle('Back to the Future');
 $backToTheFuture->year = 1985;
 $backToTheFuture->runningTime = '116 minutes';
 var_dump($backToTheFuture);
 
 $backToTheFuture2 = new Movie();
-$backToTheFuture2->title = 'Back to the Future - 2';
+$backToTheFuture2->setTitle('Back to the Future - 2');
 $backToTheFuture2->year = 1989;
 $backToTheFuture2->runningTime = '108 minutes';
 var_dump($backToTheFuture2);

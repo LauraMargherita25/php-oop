016db0e6523bd3577a5312655e09f6a61fdb159b
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-29 14:56:32 +0200

    Defined some var

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
 
 class Movie{
-
+    public $title;
+    public $year;
+    public $runningTime;
 }
 
 $backToTheFuture = new Movie();
+$backToTheFuture->title = 'Back to the Future';
+$backToTheFuture->year = 1985;
+$backToTheFuture->runningTime = '116 minutes';
+var_dump($backToTheFuture);
+
 $backToTheFuture2 = new Movie();
+$backToTheFuture2->title = 'Back to the Future - 2';
+$backToTheFuture2->year = 1989;
+$backToTheFuture2->runningTime = '108 minutes';
+var_dump($backToTheFuture2);

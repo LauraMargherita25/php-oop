c610316927543dfa08488beb5f6aa0fb4bb2a56f
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-29 17:41:05 +0200

    private var

--- index.php
+++ index.php
@@ -1,32 +1,46 @@
 <?php
 
 class Movie{
-    public $title;
+    private $title;
     public $year;
-    public $runningTime;
+    private $runningTime;
 
-    public function __construct($_title, $_year) {
+    public function __construct($_title) {
         $this->title = $_title;
-        $this->year = $_year;
+    }
+
+    public function getTitle() {
+        return $this->title;
     }
 
     public function setRunningTime($_runningTime) {
         $this->runningTime = $_runningTime;
     }
+
+    public function getRunningTime() {
+        return 'il film dura: ' . $this->runningTime;
+    }
 }
 
-// $backToTheFuture = new Movie();
-// $backToTheFuture->title = 'Back to the Future';
-// $backToTheFuture->year = 1985;
-// $backToTheFuture->setRunningTime('116 minutes');
-// var_dump($backToTheFuture);
+$backToTheFuture = new Movie('Back to the Future');
+$backToTheFuture->year = 1985;
+$backToTheFuture->getRunningTime('116 minutes');
+var_dump($backToTheFuture);
 
-// $backToTheFuture2 = new Movie();
-// $backToTheFuture2->title = 'Back to the Future - 2';
-// $backToTheFuture2->year = 1989;
-// $backToTheFuture2->setRunningTime('108 minutes');
-// var_dump($backToTheFuture2);
+$backToTheFuture2 = new Movie('Back to the Future - 2');
+$backToTheFuture2->year = 1989;
+$backToTheFuture2->setRunningTime('108 minutes');
+var_dump($backToTheFuture2);
 
-$backToTheFuture3 = new Movie("Back to the Future - 3", 1990);
-echo $backToTheFuture3->title;
+$backToTheFuture3 = new Movie("Back to the Future - 3");
+$backToTheFuture3->year = 1990;
+$backToTheFuture3->setRunningTime('108 minutes');
+// echo $backToTheFuture3->title;
 echo $backToTheFuture3->year;
+var_dump($backToTheFuture3);
+
+?>
+
+<p>
+    <?= $backToTheFuture3->getRunningTime() ?>
+</p>
